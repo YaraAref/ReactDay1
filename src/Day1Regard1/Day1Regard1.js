f12912d6e24ca90322723d4b01bebaf664a55301
Yara Awad <engyara2018@gmail.com>
2023-10-13 16:46:13 +0300

    adding files

---
+++ src/Day1Regard1/Day1Regard1.js
@@ -0,0 +1,12 @@
+import {Component} from "react";
+class Day1Regard1 extends Component{
+    render(){
+        return (
+            <div>
+                <p>welcome from statefull class component</p>  
+            </div>
+           
+        );
+    }
+}
+export default Day1Regard1;
---
+++ src/Day1Regard2/Day1Regard2.js
@@ -0,0 +1,8 @@
+function Day1Regard2(){
+    return (
+        <div>
+            <p>welcome from stateless function component</p>
+        </div>
+    );
+}
+export default Day1Regard2;
